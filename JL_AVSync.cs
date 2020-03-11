685bd809f6bcaaa29b6ee7a8b465b46f3d47f077
JohnLunaNCH <44786339+JohnLunaNCH@users.noreply.github.com>
2020-03-11 10:20:09 -0400

    Update JL_AVSync.cs

--- JL_AVSync.cs
+++ JL_AVSync.cs
@@ -1,52 +1,69 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+using System.Collections;
 using UnityEngine;
 using UnityEngine.Video;
 
 public class JL_AVSync : MonoBehaviour
 {
-    public VideoPlayer vid_lead;
-    public AudioSource aud_follow;
-    public float tolerance = 0.25f;
+    [Header("References")]
+    public VideoPlayer videoLead;
+    public AudioSource audioFollow;
+
+    [Header("Adjustments")]
+    [Tooltip("Correction occurs when audio and video are out of sync by this amount")]
+    public float tolerance = 0.04f;
+
+    [Tooltip("Use this audio source to debug how frequently the video/audio is going out of sync")]    
     public AudioSource errorSound;
+
+    [Tooltip("Start the video")]
     public float startTime = 0.0f;
+
+    [Tooltip("Use this to offset the audio from the video")]
     public float audioOffset = 0.0f;
 
     private void Start()
     {
+        //This is just for testing
         Play();
     }
 
     public void Play()
     {
-        StartCoroutine(PlayVideo());
+        StartCoroutine(PlayVideo(videoLead, audioFollow));
     }
 
-    IEnumerator PlayVideo()
+    public void Play(VideoPlayer vL, AudioSource aF)
     {
-        vid_lead.time = startTime;
-        vid_lead.Prepare();
+        StartCoroutine(PlayVideo(vL, aF));
+    }
 
-        while (!vid_lead.isPrepared)
+    IEnumerator PlayVideo(VideoPlayer lead, AudioSource follow)
+    {
+        lead.time = startTime;
+        lead.Prepare();
+
+        while (!lead.isPrepared)
         {
             yield return null;
         }
 
-        vid_lead.Play();
+        lead.Play();
 
-        aud_follow.time = startTime + audioOffset;
-        aud_follow.Play();
+        follow.time = startTime + audioOffset;
+        follow.Play();
 
-        while (vid_lead.isPlaying)
+        while (lead.isPlaying)
         {
-            if (Mathf.Abs((float)vid_lead.time - (aud_follow.time - audioOffset)) > tolerance)
+            if (Mathf.Abs((float)lead.time - (follow.time - audioOffset)) > tolerance)
             {
-                aud_follow.time = (float)vid_lead.time + audioOffset;
+                follow.time = (float)lead.time + audioOffset;
                 if (errorSound)
                     errorSound.Play();
             }
             yield return null;
         }
+        if (follow.isPlaying)
+            follow.Stop();
         yield return null;
     }
 }
